56d3b03baa805797c50fe9327e45da6087aab388
amyfuchj <amy.fuchj@gmail.com>
2024-09-29 10:48:37 +0800

    fix

--- script.js
+++ script.js
@@ -2,35 +2,56 @@
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
 let x = 0, y = 0, dx = 10, dy = 10, r = 30, color = "#0095DD";
+let canMove = 0;
 
 // 畫圓形
-function drawBall() 
+function drawBall()
 {
-	ctx.beginPath();
-	ctx.arc(x, y, r, 0, Math.PI*2); // arc(圓心x, 圓心y, 半徑, 起始角, 結束角)
-	ctx.fillStyle = color;
-	ctx.fill();
-	ctx.closePath();
+ctx.beginPath();
+ctx.arc(x, y, r, 0, Math.PI*2); // arc(圓心x, 圓心y, 半徑, 起始角, 結束角)
+ctx.fillStyle = color;
+ctx.fill();
+ctx.closePath();
 }
 
 // 按下按鍵時觸發
 document.addEventListener("keydown", keyDownHandler);
-function keyDownHandler(e) 
+function keyDownHandler(e)
 {
-	if(e.key == "ArrowRight")        x += dx;
-	else if(e.key == "ArrowLeft")    x -= dx;
+if(e.key == "ArrowRight")        x += dx;
+else if(e.key == "ArrowLeft")    x -= dx;
     else if(e.key == "ArrowUp")      y -= dy;
-	else if(e.key == "ArrowDown")    y += dy;
+else if(e.key == "ArrowDown")    y += dy;
 }
 
 // TODO: 滑鼠移動(mousemove)時觸發，改變位置(x, y)為滑鼠目前位置(e.clientX, e.clientY)
+document.addEventListener("mousemove", mousemoveHandler);
+function mousemoveHandler(e)
+{
+if(canMove)
+{
+x = e.clientX - canvas.offsetLeft;
+y = e.clientY - canvas.offsetTop;
+}
+
+}
+document.addEventListener("mousedown", mousedownHandler);
+document.addEventListener("mouseup", mouseupHandler);
+function mousedownHandler(e)
+{
+canMove = 1;
+}
+function mouseupHandler(e)
+{
+canMove = 0;
+}
 // ...
 
 
 // 更新畫布
-function draw() 
-{	
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
+function draw()
+{
+ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
     requestAnimationFrame(draw);
 }
